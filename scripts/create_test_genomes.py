d03d3ffd21036860a544c64907aab92ecb1e1193
Rob Vickerstaff <robert.vickerstaff@gmail.com>
2022-01-16 19:38:34 +0000

    now generating progeny sequences from crossing over the parent genomes

--- scripts/create_test_genomes.py
+++ scripts/create_test_genomes.py
@@ -8,6 +8,17 @@ and N simulated progeny
 import argparse
 import random
 
+class individual:
+    def __init__(self,name,seq1,seq2):
+        self.name = name
+        self.chrm = [seq1,seq2]
+
+    def __str__(self):
+        return "Name: " + self.name + "\n" + self.chrm[0][:100] + "...\n" + self.chrm[1][:100] + '...'
+
+    def get_chrm(self,i):
+        return self.chrm[i]
+
 def random_seq(size):
     'return a random sequence of the letters ATCG'
     return ''.join([random.choice("ATCG") for x in range(size)])
@@ -52,24 +63,49 @@ def generate_parents(seq,snp_list):
 
     #assign the alleles from each snp in the list
     for posn,varA,varB,pattern in snp_list:
-        print(posn,varA,varB,pattern)
-
         vars = varA+varB
         p1[0][posn] = ord(vars[int(pattern[0])])
         p1[1][posn] = ord(vars[int(pattern[1])])
         p2[0][posn] = ord(vars[int(pattern[2])])
         p2[1][posn] = ord(vars[int(pattern[3])])
 
+    p1[0] = p1[0].decode('utf-8')
+    p1[1] = p1[1].decode('utf-8')
+    p2[0] = p2[0].decode('utf-8')
+    p2[1] = p2[1].decode('utf-8')
+
     return individual("p1",p1[0],p1[1]),individual("p2",p2[0],p2[1])
 
-class individual:
-    def __init__(self,name,seq1,seq2):
-        self.name = name
-        self.chrm1 = seq1.decode('utf-8')
-        self.chrm2 = seq2.decode('utf-8')
+def generate_test_parents(size):
+    'generate parents of polyA,T,C,G for testing'
 
-    def __str__(self):
-        return "Name: " + self.name + "\n" + self.chrm1[:80] + "...\n" + self.chrm2[:80] + '...'
+    return individual("p1",'A'*size,'T'*size),individual("p2",'C'*size,'G'*size)
+
+def crossover(seq1,seq2,crossovers):
+    'return a single sequence incorporating the requests number of crossovers'
+
+    #pick positions of the crossovers
+    seqA = seq1[:]
+    seqB = seq2[:]
+    size = len(seq1)
+    posn = [x for x in range(1,size)] #[1,...size-1]
+    random.shuffle(posn)
+    posn = posn[:crossovers] #crossover before the offset position(s) given
+
+    for i in posn:
+        temp = seqA[:]
+        seqA = seqA[:i] + seqB[i:]
+        seqB = seqB[:i] + temp[i:]
+
+    return random.choice([seqA,seqB])
+
+def generate_progeny(name,p1,p2,crossovers):
+    'generate genome sequence of progeny from the parents'
+
+    seq1 = crossover(p1.get_chrm(0),p1.get_chrm(1),crossovers)
+    seq2 = crossover(p2.get_chrm(0),p2.get_chrm(1),crossovers)
+
+    return individual(name,seq1,seq2)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -77,6 +113,7 @@ if __name__ == "__main__":
     parser.add_argument('--progeny', type=int,required=True, help='Number of progeny')
     parser.add_argument('--snps', type=int,required=True, help='Number of SNPs')
     parser.add_argument('--genome-size', type=int, default=100000, help='Size of one chromosome in bases')
+    parser.add_argument('--crossovers', type=int, default=2, help='Number of crossovers')
 
     args = parser.parse_args()
 
@@ -86,8 +123,19 @@ if __name__ == "__main__":
 
     #create random chromosome of required size
     base_seq = random_seq(args.genome_size)
-    snp_list = generate_snp_list(args.genome_size,args.snps,debug=True)
 
+    #create list of snp positions, their alleles and parental states
+    snp_list = generate_snp_list(args.genome_size,args.snps)
+
+    #generate parents' genome sequence
     parent1,parent2 = generate_parents(base_seq,snp_list)
+
     print(parent1)
     print(parent2)
+
+    #generate progeny genome sequences
+    progeny_list = []
+
+    for i in range(args.progeny):
+        progeny_list.append(generate_progeny("progeny%03d"%i,parent1,parent2,args.crossovers))
+        print(progeny_list[-1])
--- test/test_create_test_genomes.sh
+++ test/test_create_test_genomes.sh
@@ -6,4 +6,4 @@
 
 set -eu
 
-../scripts/create_test_genomes.py --progeny 10 --genome-size 10000 --snps 10
+../scripts/create_test_genomes.py --progeny 10 --genome-size 100 --snps 5
